e8773196218b464b0d0668f6886bbbce8937edf3
Christina O'Donnell <cdo@mutix.org>
2024-10-26 14:41:40 +0100

    Add initial pyproject and flake.nix

---
+++ flake.nix
@@ -0,0 +1,33 @@
+{
+  description = "FinanceForge";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    pyproject.url = "github:nix-community/pyproject.nix";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, pyproject }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+        python = pkgs.python3;
+        project = pyproject.lib.project.loadPyproject {
+          projectRoot = ./.;
+          format = "pyproject";
+        };
+      in {
+        devShells.default = pkgs.mkShell {
+          packages = [ python.withPackages (ps: [ ps.flask ]) ];
+        };
+
+        packages.default = pkgs.python3Packages.buildPythonApplication {
+          pname = "financeforge";
+          version = "0.1.0";
+          src = ./.;
+          format = "pyproject";
+          propagatedBuildInputs = [ pkgs.python3Packages.flask ];
+        };
+      }
+    );
+}
